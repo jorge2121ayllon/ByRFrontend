ea53a1d473dd4637cbe61a4b3a098195c1d428d1
jorge2121ayllon <jorgeayllon2121@gmail.com>
2020-09-25 20:09:04 -0400

    add changes

--- ClientApp/src/app/modelos/pageandsortResponse.model.ts
+++ ClientApp/src/app/modelos/pageandsortResponse.model.ts
@@ -1,7 +1,4 @@
-import { UsuarioDetalle } from '../shared/usuario-detalle.model';
-import { Empresa } from './empresa.model';
 
 export class PageandsortResponse {
     TotalFilas: number;
-    Datos: Empresa[];
 }
